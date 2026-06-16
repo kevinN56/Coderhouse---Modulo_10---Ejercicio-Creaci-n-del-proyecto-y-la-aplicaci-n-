1a0977abc6cf269b8b7184b67521f72f91cd3239
kevinN <kevinnishi56@gmail.com>
2026-06-16 12:51:23 -0300

    modific models.py and admin.py

--- workshop/core/admin.py
+++ workshop/core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Autor,Post,Tag
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title',)
+    list_display = ('title','author','published_date')
     list_filter = ('tags',)
 admin.site.register(Post,PostAdmin)    
 admin.site.register(Autor)
--- workshop/core/models.py
+++ workshop/core/models.py
@@ -1,20 +1,21 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 from django.core.exceptions import ValidationError
-# Create your models here.
+
 class Autor(models.Model):
-    name = models.CharField(max_length=100, null = False)
+    name = models.CharField(max_length=100, null = False,validators=[MinLengthValidator(3)])
     email = models.EmailField(unique=True, null = False)
     
     def __str__(self):
         return self.name
 
 class Tag(models.Model):
-    name = models.CharField(max_length = 30)
+    name = models.CharField(max_length = 30,validators=[MinLengthValidator(2)])
     def __str__(self):
         return self.name
     
 class Post(models.Model):
-    title = models.CharField(max_length=200,verbose_name='Titulo')
+    title = models.CharField(max_length=200,verbose_name='Titulo',validators=[MinLengthValidator(5)])
     content = models.TextField(verbose_name='Contenido')
     published_date = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Publicacion')
     author = models.ForeignKey(Autor, on_delete=models.CASCADE,verbose_name="Autor")
@@ -25,7 +26,6 @@ class Post(models.Model):
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
         ordering = ['-published_date']
-# En el modelo Post, implementar el método clean() para validar que el título contenga al menos 5 caracteres.        
     def clean(self):
         if len(self.title) < 5:
             raise ValidationError('EL titulo debe contener al menos 5 Letras')
