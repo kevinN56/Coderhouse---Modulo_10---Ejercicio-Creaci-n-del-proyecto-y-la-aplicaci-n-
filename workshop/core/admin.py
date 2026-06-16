5c661dd38d13ac94105c42f3c692882a85792b1b
kevinN <kevinnishi56@gmail.com>
2026-06-16 12:07:43 -0300

    Modificacion de models.py y admin.py

--- workshop/core/admin.py
+++ workshop/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Autor,Post,Tag
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('tags',)
+admin.site.register(Post,PostAdmin)    
+admin.site.register(Autor)
+admin.site.register(Tag)
